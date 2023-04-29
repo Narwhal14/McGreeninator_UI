66dd7e7c9711f3973f3159f328055a4522a74015
narwhal <btwells18@gmail.com>
2023-04-29 10:13:25 -0700

    initial commit

---
+++ Window/MainWindow.axaml.cs
@@ -0,0 +1,20 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace McGreeninator_UI
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // buttons
+
+        public void btn_Settings_Click(object sender, RoutedEventArgs e)
+        {
+            // launches the settings panel
+        }
+    }
+}
